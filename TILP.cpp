4609d37e6d29ab81e933999d97eaefbe626d9c10
Christopher Mitchell <github@cemetech.net>
2014-10-15 01:36:52 -0400

    Initial translation from old monolithic library

---
+++ TILP.cpp
@@ -0,0 +1,215 @@
+/*************************************************
+ *  TILP.h - Library for linking TI calculators  *
+ *           and Arduinos.                       *
+ *           Created by Christopher Mitchell,    *
+ *           2011-2014, all rights reserved.     *
+ *************************************************/
+
+#include "Arduino.h"
+#include "TILP.h"
+
+TILP::TILP(***REMOVED*** {
+	tip_ = DEFAULT_TIP;
+	ring_ = DEFAULT_RING;
+}
+
+TILP::TILP(int tip, int ring***REMOVED*** {
+	tip_ = tip;
+	ring_ = ring;
+}
+
+// Send an entire message from the Arduino to
+// the attached TI device, byte by byte
+int TILP::send(uint8_t* header, uint8_t* data, int datalength***REMOVED*** {
+	long previousMillis = 0;
+
+	// Send all of the bytes in the header
+	for(int idx = 0; idx < 4; idx++***REMOVED*** {
+		int rval = sendByte(header[idx]***REMOVED***;
+		if (rval != 0***REMOVED***
+			return rval;
+	}
+	
+	// If no data, we're done
+	if (datalength == 0***REMOVED***
+		return 0;
+	
+	// These  also indicate that there are 
+	// no data bytes to be sent
+	if (header[1] == CTS ||
+		header[1] == VER ||
+		header[1] == ACK ||
+		header[1] == ERR ||
+		header[1] == RDY ||
+		header[1] == SCR ||
+		header[1] == KEY ||
+		header[1] == EOT***REMOVED***
+	{
+		return 0;
+	}
+	
+	// Send all of the bytes in the data buffer
+	uint16_t checksum = 0;
+	for(int idx = 0; idx < datalength; idx++***REMOVED*** {
+		// Try to send this byte
+		int rval = sendByte(data[idx]***REMOVED***;
+		if (rval != 0***REMOVED***
+			return rval;
+		checksum += data[idx];
+	}
+	
+	// Send the checksum
+	int rval = sendByte(checksum & 0x00ff***REMOVED***;
+	if (rval != 0***REMOVED***
+		return rval;
+	rval = sendByte((checksum >> 8***REMOVED*** & 0x00ff***REMOVED***;
+	return rval;
+}
+
+// Send a single byte from the Arduino to the attached
+// TI device, returning nonzero if a failure occurred.
+int TILP::sendByte(uint8_t byte***REMOVED*** {
+	long previousMillis = 0;
+
+	// Send all of the bits in this byte
+	for(int bit = 0; bit < 8; bit++***REMOVED*** {
+		
+		// Wait for both lines to be high before sending the bit
+		previousMillis = 0;
+		while (digitalRead(ring_***REMOVED*** == LOW || digitalRead(tip_***REMOVED*** == LOW***REMOVED*** {
+			if (previousMillis++ > TIMEOUT***REMOVED***
+				return ERR_WRITE_TIMEOUT;
+		}
+		
+		// Pull one line low to indicate a new bit is going out
+		bool bitval = (byte & 1***REMOVED***;
+		int line = (bitval***REMOVED***?ring_:tip_;
+		pinMode(line, OUTPUT***REMOVED***;
+		digitalWrite(line, LOW***REMOVED***;
+		
+		// Wait for peer to acknowledge by pulling opposite line low
+		line = (bitval***REMOVED***?tip_:ring_;
+		previousMillis = 0;
+		while (digitalRead(line***REMOVED*** == HIGH***REMOVED*** {
+			if (previousMillis++ > TIMEOUT***REMOVED***
+				return ERR_WRITE_TIMEOUT;
+		}
+
+		// Wait for peer to indicate readiness by releasing that line
+		resetLines(***REMOVED***;
+		previousMillis = 0;
+		while (digitalRead(line***REMOVED*** == LOW***REMOVED*** {
+			if (previousMillis++ > TIMEOUT***REMOVED***
+				return ERR_WRITE_TIMEOUT;
+		}
+		
+		// Rotate the next bit to send into the low bit of the byte
+		byte >>= 1;
+	}
+	
+	return 0;
+}
+
+// Returns 0 for a successfully-read message or non-zero
+// for failure. If return value is 0 and datalength is zero,
+// then the message is just a 4-byte message in the header
+// buffer. If the 
+int TILP::get(uint8_t* header, uint8_t* data, int* datalength, int maxlength***REMOVED*** {
+	int rval;
+
+	// Get the 4-byte header: sender, message, length
+	for(int idx = 0; idx < 4; idx++***REMOVED*** {
+		rval = getByte(&header[idx]***REMOVED***;
+		if (rval***REMOVED***
+			return rval;
+	}
+	
+	// Check if this is a data-free message
+	*datalength = (int***REMOVED***header[2] | ((int***REMOVED***header[3] << 8***REMOVED***;
+	if (*datalength == 0***REMOVED***;
+		return 0;
+	if (*datalength > maxlength***REMOVED***
+		return ERR_BUFFER_OVERFLOW;
+	
+	// These  also indicate that there are 
+	// no data bytes to be received
+	if (header[1] == CTS ||
+		header[1] == VER ||
+		header[1] == ACK ||
+		header[1] == ERR ||
+		header[1] == RDY ||
+		header[1] == SCR ||
+		header[1] == KEY ||
+		header[1] == EOT***REMOVED***
+	{
+		return 0;
+	}
+	
+	// Get the data bytes, if there are any.
+	uint16_t checksum = 0;
+	for(int idx = 0; idx < *datalength; idx++***REMOVED*** {
+		// Try to get all the bytes, or fail if any of the
+		// individual byte reads fail
+		rval = getByte(&data[idx]***REMOVED***;
+		if (rval != 0***REMOVED***
+			return rval;
+			
+		// Update checksum
+		checksum += data[idx];
+	}
+	
+	// Receive and check the checksum
+	uint8_t recv_checksum[2];
+	for(int idx = 0; idx < 2; idx++***REMOVED*** {
+		rval = getByte(&recv_checksum[idx]***REMOVED***;
+		if (rval***REMOVED***
+			return rval;
+	}
+	
+	// Die on a bad checksum
+	if (checksum != (uint8_t***REMOVED***(((int***REMOVED***recv_checksum[1] << 8***REMOVED*** | (int***REMOVED***recv_checksum[0]***REMOVED******REMOVED***
+		return ERR_BAD_CHECKSUM;
+		
+	return 0;
+}
+
+int TILP::getByte(uint8_t* byte***REMOVED*** {
+	long previousMillis = 0;
+	*byte = 0;
+	
+	// Pull down each bit and store it
+	for (int bit = 0; bit < 8; bit++***REMOVED*** {
+		int linevals;
+
+		previousMillis = 0;
+		while ((linevals = (digitalRead(ring_***REMOVED*** << 1 | digitalRead(tip_***REMOVED******REMOVED******REMOVED*** == 0x03***REMOVED*** {
+			if (previousMillis++ > GET_ENTER_TIMEOUT***REMOVED***
+				return ERR_READ_TIMEOUT;
+		}
+		
+		// Store the bit, then acknowledge it
+		*byte = (*byte >> 1***REMOVED*** | ((linevals == 0x01***REMOVED***?0x80:0x7f***REMOVED***;
+		int line = (linevals == 0x01***REMOVED***?tip_:ring_;
+		pinMode(line, OUTPUT***REMOVED***;
+		digitalWrite(line, LOW***REMOVED***;
+		
+		// Wait for the peer to indicate readiness
+		line = (linevals == 0x01***REMOVED***?ring_:tip_;		
+		previousMillis = 0;
+		while (digitalRead(line***REMOVED*** == LOW***REMOVED*** {            //wait for the other one to go low
+			if (previousMillis++ > TIMEOUT***REMOVED***
+				return ERR_READ_TIMEOUT;
+		}
+		digitalWrite(line,HIGH***REMOVED***;
+		
+		resetLines(***REMOVED***;
+	}
+	return 0;
+}
+
+void TILP::resetLines(void***REMOVED*** {
+	pinMode(ring_, INPUT***REMOVED***;           // set pin to input
+	digitalWrite(ring_, HIGH***REMOVED***;       // turn on pullup resistors
+	pinMode(tip_, INPUT***REMOVED***;            // set pin to input
+	digitalWrite(tip_, HIGH***REMOVED***;        // turn on pullup resistors
+}
---
+++ demos/ControlLED/ControlLED.ino
@@ -0,0 +1,18 @@
+#include "TILP.h"
+
+TILP* tilp;
+int lineRed = 2;
+int lineWhite = 3;
+
+void setup(***REMOVED*** {
+  tilp = new TILP(lineRed, lineWhite***REMOVED***;
+  tilp->resetLines(***REMOVED***;
+}
+
+void loop(***REMOVED*** {
+  uint8_t header[4] = {COMP83P, KEY, 0xA6, 0x00};
+  int rlen = 0;
+  tilp->send(header, NULL, 0***REMOVED***;
+  tilp->get(header, NULL, &rlen, 0***REMOVED***;
+}
+
