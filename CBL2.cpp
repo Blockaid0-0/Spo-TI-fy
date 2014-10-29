9aa73a631d884cbef0730677fabae2178a3c7fc8
Christopher Mitchell <github@cemetech.net>
2014-10-29 11:59:21 -0400

    Added message state machine components for calc->CBL2 data transfers

--- CBL2.cpp
+++ CBL2.cpp
@@ -46,8 +46,63 @@ int CBL2::setupCallbacks(uint8_t* header, uint8_t* data, int maxlength,
 }
 
 int CBL2::eventLoopTick(***REMOVED*** {
+	uint8_t msg_header[4];
+	int length;
+	int rval;
+	int endpoint = 0x12;
+
 	if (!callback_init***REMOVED***
 		return -1;
+	
+	// See if there's a message coming
+	int rval = get(msg_header, data_, length, maxlength_***REMOVED***;
+	if (rval***REMOVED***
+		return;			// No message coming
 		
+	// Deduce what kind of operation is happening
+	// CBL2 responds to TI-82 as 0x12, "0x95" endpoint as 0x15
+	endpoint = (msg_header[0] == COMP82***REMOVED***?0x12:0x15;
+	
+	// Now deal with the message
+	switch(msg_header[1]***REMOVED*** {
+		case ACK:
+			break;						// Drop ACKs on the floor
+
+		case RTS:
+			memcpy(header_, data_, length***REMOVED***;		// Save the variable header
+			
+			// Send an ACK
+			msg_header[0] = endpoint;
+			msg_header[1] = ACK;
+			msg_header[2] = msg_header[3] = 0x00;
+			send(msg_header, NULL, 0***REMOVED***;
+			
+			// Send a CTS
+			msg_header[0] = endpoint;
+			msg_header[1] = CTS;
+			msg_header[2] = msg_header[3] = 0x00;
+			send(msg_header, NULL, 0***REMOVED***;
+			
+			break;
+		
+		case DATA:
+			// Send an ACK
+			msg_header[0] = endpoint;
+			msg_header[1] = ACK;
+			msg_header[2] = msg_header[3] = 0x00;
+			send(msg_header, NULL, 0***REMOVED***;
+			
+			// Deliver the data to the callback
+			rval = get_callback_(header_[1], length***REMOVED***;		// Ignore rval for now	
+			break;
+	
+		case EOT:
+			// Send an ACK
+			msg_header[0] = endpoint;
+			msg_header[1] = ACK;
+			msg_header[2] = msg_header[3] = 0x00;
+			send(msg_header, NULL, 0***REMOVED***;
+			break;
+			
 	return -1;
 }
