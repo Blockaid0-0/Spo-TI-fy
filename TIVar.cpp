ca80c8ef3ea7bbd637d54b9eca0d5ed648f20590
Christopher Mitchell <github@cemetech.net>
2014-11-01 01:59:04 -0400

    Repaired problems with TIVar::floatToReal8x after testing

--- TIVar.cpp
+++ TIVar.cpp
@@ -42,7 +42,7 @@ double TIVar::realToFloat8x(uint8_t* real***REMOVED*** {
 	return ieee_acc;
 }
 
-int floatToReal8x(double f, uint8_t* real***REMOVED*** {
+int TIVar::floatToReal8x(double f, uint8_t* real***REMOVED*** {
 	int16_t exp = 0;
 	
 	// Set sign bit and get absolute value
@@ -50,18 +50,21 @@ int floatToReal8x(double f, uint8_t* real***REMOVED*** {
 	f = (f > 0***REMOVED***?f:-f;
 	
 	// Bring large numbers down
-	while(f != 0 && f >= 10.e14***REMOVED*** {
+	while(f != 0 && f >= 10.e13***REMOVED*** {
 		f /= 10.f;
+		exp += 1;
 	}
 	
 	// Bring small numbers up
-	while(f != 0 && f < 1.e14***REMOVED*** {
+	while(f != 0 && f < 1.e13***REMOVED*** {
 		f *= 10.f;
+		exp -= 1;
 	}
 	
 	// Extract the digits
-	for(uint8_t i=13; i >= 0; i--***REMOVED*** {
-		double digit = fmod(f, 10.***REMOVED***;
+	for(int8_t i=13; i >= 0; i--***REMOVED*** {
+        double digit, odigit;
+        digit = odigit = fmod(f, 10.***REMOVED***;
 		uint8_t cdigit = 0;
 		while(digit > 0.5***REMOVED*** {
 			cdigit++;
@@ -69,15 +72,15 @@ int floatToReal8x(double f, uint8_t* real***REMOVED*** {
 		}
 		
 		if ((i & 0x01***REMOVED*** == 1***REMOVED*** {
-			real[2 + (i >> 2***REMOVED***] = cdigit;
+			real[2 + (i >> 1***REMOVED***] = cdigit;
 		} else {
-			real[2 + (i >> 2***REMOVED***] |= (cdigit << 4***REMOVED***;
+			real[2 + (i >> 1***REMOVED***] |= (cdigit << 4***REMOVED***;
 		}
-		f = (f - digit***REMOVED*** / 10.f;
+		f = (f - odigit***REMOVED*** / 10.f;
 	}
 	
 	// Set the exponent
-	exp += 0x80;
+	exp += 0x80 + 13;
 	real[1] = (uint8_t***REMOVED***exp;
 
 	return 0;		// Success
