62a7c4538d71f29b627da8c1d686c717ab33b39f
Christopher Mitchell <github@cemetech.net>
2015-10-11 16:41:10 -0400

    Added more verbose comments to ControlLED demo

--- examples/ControlLED/ControlLED.ino
+++ examples/ControlLED/ControlLED.ino
@@ -24,6 +24,8 @@ CBL2* cbl;
 const int lineRed = DEFAULT_TIP;
 const int lineWhite = DEFAULT_RING;
 
+// Specify the number of LEDs to control, and to which
+// digital pin each LED is attached.
 #if defined(__MSP432P401R__***REMOVED***		// MSP432 target
 #define LED_PIN_COUNT 4
 const int ledPins[LED_PIN_COUNT] = {78, 75, 76, 77};
@@ -32,24 +34,39 @@ const int ledPins[LED_PIN_COUNT] = {78, 75, 76, 77};
 const int ledPins[LED_PIN_COUNT] = {8, 9, 10, 13};
 #endif
 
+// Lists are 2 + (9 * dimension***REMOVED*** bytes,
+// so incidentally a 255-byte max data length
+// limits this demo's lists to 28 elements.
 #define MAXDATALEN 255
+
 uint8_t header[16];
 uint8_t data[MAXDATALEN];
 
+// Run once when the Arduino resets. This first sets
+// the LED pins to OUTPUT, then initializes a new CBL2
+// instance. This is responsible for dealing with the
+// actual calculator<->Arduino message exchange.
 void setup(***REMOVED*** {
   for(int i = 0; i < LED_PIN_COUNT; i++***REMOVED*** {
     pinMode(ledPins[i], OUTPUT***REMOVED***;
 	digitalWrite(ledPins[i], LOW***REMOVED***;
   }
 
-  Serial.begin(9600***REMOVED***;
+  Serial.begin(9600***REMOVED***;                           // Used for debugging
   cbl = new CBL2(lineRed, lineWhite***REMOVED***;
   cbl->resetLines(***REMOVED***;
-  // cbl->setVerbosity(true, &Serial***REMOVED***;			// Comment this in for mesage information
+  // cbl->setVerbosity(true, &Serial***REMOVED***;			// Comment this in for verbose message information
+  
+  // The following registers buffers for exchanging data, the maximum
+  // allowed data length, and functions to call on Get(***REMOVED*** and Send(***REMOVED***.
   cbl->setupCallbacks(header, data, MAXDATALEN,
                       onGetAsCBL2, onSendAsCBL2***REMOVED***;
 }
 
+// Repeatedly check to see if the calculator has initiated a Get(***REMOVED***
+// or a Send(***REMOVED*** operation yet. If it has, then onGetAsCBL2(***REMOVED*** or
+// onSendAsCBL2(***REMOVED*** will be invoked, since they were registered in
+// setup(***REMOVED*** above.
 void loop(***REMOVED*** {
   int rval;
   rval = cbl->eventLoopTick(***REMOVED***;
@@ -59,20 +76,23 @@ void loop(***REMOVED*** {
   }
 }
 
+// Callback when the CBL2 class has successfully received a variable
+// from the attached calculator.
 int onGetAsCBL2(uint8_t type, enum Endpoint model, int datalen***REMOVED*** {
   Serial.print("Got variable of type "***REMOVED***;
   Serial.print(type***REMOVED***;
   Serial.print(" from endpoint of type "***REMOVED***;
   Serial.println((int***REMOVED***model***REMOVED***;
   
+  // We only want to handle lists.
   if (type != VarTypes82::VarRList***REMOVED***
     return -1;
 
   // Turn the LEDs on or off
   uint16_t list_len = sizeWordToInt(&(data[0]***REMOVED******REMOVED***;			// Convert 2-byte size word to int
   if (list_len == 1***REMOVED*** {
-    // It is a 1-element list now
-	int value = (int***REMOVED***TIVar::realToFloat8x(&data[2], model***REMOVED***;	// First list element starts here
+    // It is indeed a 1-element list
+	int value = (int***REMOVED***TIVar::realToFloat8x(&data[2], model***REMOVED***;	// First list element starts after 2-byte size word
     Serial.print("Received value "***REMOVED***;
     Serial.println(value***REMOVED***;
     for(int i = 0; i < LED_PIN_COUNT; i++***REMOVED*** {
@@ -84,6 +104,9 @@ int onGetAsCBL2(uint8_t type, enum Endpoint model, int datalen***REMOVED*** {
   return 0;
 }
 
+// Callback when the CBL2 class notices the attached calculator
+// wants to start a Get(***REMOVED*** exchange. The CBL2 class needs to get
+// any data to send before continuing the exchange.
 int onSendAsCBL2(uint8_t type, enum Endpoint model, int* headerlen,
                  int* datalen, data_callback* data_callback***REMOVED***
 {
@@ -91,5 +114,5 @@ int onSendAsCBL2(uint8_t type, enum Endpoint model, int* headerlen,
   Serial.print(type***REMOVED***;
   Serial.print(" from endpoint of type "***REMOVED***;
   Serial.println((int***REMOVED***model***REMOVED***;
-  return -1;
+  return -1;		// -1 means we have no data to send.
 }
