358a391a06e57102f248701d1f53550069d3355e
Blockaid <blake.halder@tolarschools.org>
2026-09-16 12:47:32 -0500

    Updated main.asm

--- src/z80/main.asm
+++ src/z80/main.asm
@@ -9,30 +9,85 @@ Init:
    ret
    jr Start
 
-   .dw $0000         ;or .dw $0000 if you don't have a description
-   .db $07,$00         ;always this string
-   .dw $0000            ;or .dw $0000 if you don't have an icon
-   .dw $0000             ;usually .dw $0000 if you don't have or know what an ALE is
-Start:                             ;main routines
+   .dw $0000
+   .db $07,$00
+   .dw $0000
+   .dw $0000
+Start:
+   ;ld hl, stringName
+   ;bcall(_Mov9ToOP1)
+   ;bcall(_ChkFindSym)
+   ;JR     C, String
+   ;bcall(_DelVarArc)
+
    call OpenGUIStack
    ld hl, mainWindow
    ld de, mainWindowE-mainWindow
    ld a, GUIRLargeWin
    call PushGUIStack
+
+   ld hl, firstText
+   ld de, firstTextE-firstText
+   ld a, GUIRText
+   call PushGUIStack
+
+   ld hl, secondText
+   ld de, secondTextE-secondText
+   ld a, GUIRText
+   call PushGUIStack
+
+   ld hl, thirdText
+   ld de, thirdTextE-thirdText
+   ld a, GUIRText
+   call PushGUIStack
+
    call RenderGUI
    call PopGUIStacks
    bcall(_GetKey)
    cp $09
    jp z, exit
    jp Start
+
 mainWindow:
-	.db %01111100
-	.db %01000100
-	.db %01000100
-	.db %01000100
-	.db %11001100
+	.db %01111000
+	.db %01001000
+	.db %01001000
+	.db %01001000
+	.db %11011000
 	.db " Spo(TI)fy",0
 mainWindowE:
+firstText:
+	.db 4
+	.db 2
+	.db 0
+	.db "Song",0
+firstTextE:
+secondText:
+	.db 4
+	.db 10
+	.db 0
+	.db "Artist",0
+secondTextE:
+thirdText:
+	.db 4
+	.db 18
+	.db 0
+	.db "Time",0
+thirdTextE:
+;String:
+;   ld hl, stringDataE-stringData
+;   bcall(_CreateStrng)
+;   inc de
+;   inc de
+;   ld hl, stringData
+;   ld bc, stringDataE-stringData
+;   ldir
+;   ret
+;stringName:
+;   .db StrngObj,tVarStrng,tStr1,0
+;stringData:
+;   .dw "FINALLY",0
+;stringDataE:
 exit:
     ret
     call CloseGUIStack
